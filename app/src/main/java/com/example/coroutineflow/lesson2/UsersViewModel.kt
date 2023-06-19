87c87e69b8df56b1927dca718190bd25986f03e9
Damdin <damdin.sandanov2@bk.ru>
2023-06-19 17:27:48 +0300

    Lessons 2

---
+++ app/src/main/java/com/example/coroutineflow/lesson2/UsersViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.coroutineflow.lesson2
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.launch
+
+class UsersViewModel : ViewModel() {
+    private val repository = UsersRepository
+    private val _users = MutableLiveData<List<String>>()
+    val users: LiveData<List<String>> = _users
+    init {
+        loadUsers()
+    }
+    fun addUser(user: String) {
+        viewModelScope.launch {
+            repository.addUser(user)
+        }
+    }
+    private fun loadUsers() {
+        viewModelScope.launch {
+            repository.loadUsers().collect() {
+                _users.value = it
+            }
+        }
+    }
+}
