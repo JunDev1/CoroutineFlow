164ff921b26ce7c2801864932bc96c4da886ed0a
Damdin <damdin.sandanov2@bk.ru>
2023-06-21 18:42:42 +0300

    Lessons 6

---
+++ app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.coroutineflow.crypto_app
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+class CryptoViewModel : ViewModel() {
+    private val repository = CryptoRepository
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter { it.isNotEmpty() }
+        .map { State.Content(it) as State }
+        .onStart { emit(State.Loading) }
+        .asLiveData()
+}
